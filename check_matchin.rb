a18819f36afe565f0e8ac1c1c717bc99b02bcc4d
yusais_life@qq.com <39078847+helloyuzz@users.noreply.github.com>
2020-06-12 15:49:27 +0800

    Create check_matchin.rb

---
+++ check_matchin.rb
@@ -0,0 +1,63 @@
+def check_in(item)
+    if /lab/ =~ item
+        puts item
+    else
+        puts "no match"
+    end
+end
+
+check_in("laboratory")
+check_in("experiment")
+check_in("Pans Labyrinth")
+check_in("elaborate")
+check_in("polar bear")
+
+def execute(&block)
+    block.call
+  end
+  
+execute { puts "Hello from inside the execute method!" }
+
+
+## block arguments，也就是将一段代码，或者函数，作为参数进行传递
+def show_blockaa(&block)
+    block.call
+end
+
+show_blockaa{puts "aaaaaaaaaaa"}
+
+def puts_aa()
+    puts "aaa-" * 4
+end
+
+
+def show_block1(&block)
+    block.call
+end
+show_block1{puts_aa()}
+
+# puts "aaa"
+
+a = "aa"
+a << "bb"
+a << 1
+print(a)
+
+puts()
+class AA
+    @@id=1
+    @@type='a'
+    def initialize(id,type)
+      @id,type = id,type
+    end
+    def show
+        puts ''
+        puts "id=",@id,"type=",@type
+    end
+end
+
+
+aa = AA.new(1,'aa')
+# print(aa)
+puts('-' * 40)
+puts(aa.show)
