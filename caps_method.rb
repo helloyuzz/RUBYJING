71c1212f91a8c105c59f6bdddd90434d6eb22ebf
yusais_life@qq.com <39078847+helloyuzz@users.noreply.github.com>
2020-06-17 14:45:07 +0800

    Create caps_method.rb

---
+++ caps_method.rb
@@ -0,0 +1,12 @@
+# caps_method.rb
+
+def caps(string)
+    if string.length > 10
+      string.upcase
+    else
+      string
+    end
+  end
+  
+  puts caps("Joe Smith")
+  puts caps("Joe Robertson")
